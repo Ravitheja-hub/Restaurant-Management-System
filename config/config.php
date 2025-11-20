56eb517f7148a7bce88044f7e56a068eefaa3e89
Ravitheja-hub <ravithejayg@gmail.com>
2025-11-20 21:43:59 +0530

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+if (!defined("HOST")) {
+    define("HOST", "127.0.0.1");
+    define("PORT", "3307");
+    define("DBNAME", "restoran");
+    define("USER", "root");
+    define("PASS", "ravitheja@123");
+}
